4e550bc0adc36712c5c69f1bbded2c91251c169a
ell-ska <ella@skagerstrom.se>
2024-10-11 11:06:35 +0200

    fix search container

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -2,16 +2,18 @@ import { Search } from 'lucide-react'
 
 export const SearchBar = () => {
   return (
-    <div className='flex w-full max-w-lg items-center gap-2 rounded-full bg-white px-4 py-2 text-zinc-400'>
-      <label htmlFor='search'>
-        <Search size={20} />
-      </label>
-      <input
-        id='search'
-        type='text'
-        placeholder='search...'
-        className='text-zinc-800 outline-none'
-      />
+    <div className='relative w-full max-w-lg'>
+      <div className='flex w-full items-center gap-2 rounded-full bg-white px-4 py-2 text-zinc-400'>
+        <label htmlFor='search'>
+          <Search size={20} />
+        </label>
+        <input
+          id='search'
+          type='text'
+          placeholder='search...'
+          className='text-zinc-800 outline-none'
+        />
+      </div>
     </div>
   )
 }
